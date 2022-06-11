62ccd25c92d1f084b23bb06497ed9aa004a5a55e
leauny <leauny@outlook.com>
2022-06-11 22:27:26 +0800

    进程树建立代码完成; 进程树打印代码完成基础版.

--- m1_pstree/pstree.c
+++ m1_pstree/pstree.c
@@ -54,39 +54,126 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+void printprocList(procNode* ptr){
+    // 打印进程名称列表
+    while(ptr != NULL){
+        printf("%6d - %d (%s)\n", ptr->ppid, ptr->pid, ptr->name);
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
+
 
 int pstree(int showPid, int pidSort){
 
     struct Node* procList = getProc();
+    
+    procNode* ptr = &procList->head; // 方便连接移走的链表结点
+    procNode* root = (procNode*)malloc(sizeof(procNode));
+    strcpy(root->name, "root"); root->next=NULL; root->nextLevel=NULL; root->pid=0; root->ppid=-1;
 
-    // 打印进程名称列表
-    procNode* ptr = procList->head.next;
-    while(ptr->next != &procList->tail){
-        printf("%s(%d)\n", ptr->name, ptr->pid);
-        ptr = ptr->next;
-    }
+    formationTree(procList, root);
 
-    // 打印进程树
-    procTree(showPid, pidSort);
+    printTree(root->nextLevel, 0);
 
-    if(pidSort == 1){
-        printf("\n\nsorting ...\n\n");
-    }
     return 0;
 }
 
-void procTree(int showPid, int pidSort){
-    printf("\nsystemd\n\tagent\n\t\thello\n\tclsdfasdfasash\n\t\tnode\n\thi\n");
+void printTree(procNode* root, int deepth){
+    if(root == NULL){
+        return;
+    }
+
+    procNode* ptr = root;
+
+    while(ptr != NULL){
+        for(int i = deepth; i > 0; i--){
+            printf("\t");
+        }
+        printf("%s(%d)\n", ptr->name, ptr->pid);
+        
+        if(ptr->nextLevel != NULL){
+            printTree(ptr->nextLevel, deepth + 1);
+        }
+        ptr = ptr->next;
+    }
+}
+
+void formationTree(struct Node* procList, procNode* currentProc){
+    // 如果没有待处理的, 则退出
+    procNode* ptr = &procList->head;
+    procNode* cptr = currentProc;
+
+    procNode* newCurrentProc = NULL;
+
+    while(ptr->next != NULL){ 
+        /*
+        // bug: 在找孩子时被拐走
+        if(currentProc->pid == ptr->next->ppid){
+            // 找到当前结点的孩子
+            printf("+ %s(%d)\n", ptr->next->name, ptr->next->pid);
+            newCurrentProc = ptr->next; //　保存当前节点地址
+            // currentProc->next = newCurrentProc;
+
+            ptr->next = ptr->next->next; // 连接主链
+            newCurrentProc->next = NULL;
+            
+            // 当前匹配结点加入兄弟组, 匹配结点的nextLevel进行递归查找
+            currentProc->nextLevel = newCurrentProc;
+            formationTree(procList, newCurrentProc);
+        }else if(currentProc->ppid == ptr->next->ppid){
+            // 找到兄弟
+            printf("- %s(%d)\n", ptr->next->name, ptr->next->pid);
+            currentProc->next = ptr->next;
+            currentProc = currentProc->next; // 移动current防止丢失信息
+            ptr->next = ptr->next->next; // 连接主链
+            currentProc->next = NULL;
+        }
+        else{
+            printf("\t^ %s(%d)\n", ptr->next->name, ptr->next->pid);
+            ptr = ptr->next;
+        }*/
+
+        if(currentProc->ppid == ptr->next->ppid){
+            // 先保存所有的同级结点, 再进入每一个的子节点找孩子
+            currentProc->next = ptr->next;
+            currentProc = currentProc->next; // 移动current防止丢失信息
+            ptr->next = ptr->next->next; // 连接主链
+            currentProc->next = NULL;
+        }else{
+            ptr = ptr->next;
+        }
+    }
+
+    while(cptr != NULL){
+
+        ptr = &procList->head;
+
+        while(ptr->next != NULL){
+            if(cptr->pid == ptr->next->ppid){
+                // 找到当前结点的孩子
+                cptr->nextLevel = ptr->next; //　保存当前节点地址
+                ptr->next = ptr->next->next; // 连接主链
+                cptr->nextLevel->next = NULL;
+                formationTree(procList, cptr->nextLevel);
+            }else{
+                ptr = ptr->next;
+            }
+        }
+        cptr = cptr->next;
+    }
 }
 
 struct Node* getProc(){
     /*
         获取进程的信息, 保存到struct结构体内
     */
+
     // 链表头尾结点结构 &初始化
     struct Node *procList = (struct Node*)malloc(sizeof(struct Node)); 
-    procList->head.next = &procList->tail;
-    procList->tail.next = NULL;
+    procList->head.next = NULL;// &procList->tail;
+    // procList->tail.next = NULL;
+    procNode *tailptr = &procList->head; // 尾插法指针
 
     DIR *dir = opendir("/proc/"); // 记得closedir
 
@@ -108,33 +195,42 @@ struct Node* getProc(){
         
         // 创建结点并连接
         procNode* node = (procNode*) malloc(sizeof(procNode));
-        node->next = procList->head.next;
-        procList->head.next = node;
+        tailptr->next = node;
+        node->nextLevel = NULL;
+        node->next = NULL;// &procList->tail;
         
         if(freopen(dirstr, "r", stdin) != NULL){
             // 获取进程信息, 并存入结构体内
-            scanf("Name:\t%s\nUmask:\t%d\nState:\t%c%s\nTgid:\t%d\nNgid:\t%d\nPid:\t%d\nPPid:\t%d", node->name, &umask, &state, dirstr, &node->pid, &node->pid, &node->pid, &node->ppid);
-            if(atoi(pdir->d_name) != node->pid || node->ppid == 0 || state == 'I' || strcmp(node->name, "sleep") == 0 || umask != 22){
+            scanf("Name: %[^\n] Umask: %d State: %c %[^\n] Tgid: %d Ngid: %d Pid: %d PPid: %d", node->name, &umask, &state, dirstr, &node->pid, &node->pid, &node->pid, &node->ppid);
+
+            /*
+                阻止部分进程会导致pstree不完整
+                if((atoi(pdir->d_name) != node->pid || node->ppid == 0 || state == 'I' || strcmp(node->name, "sleep") == 0 || umask != 22) && node->pid != 1)
+            */
+            if((atoi(pdir->d_name) != node->pid || node->ppid == 0 || state == 'I') && node->pid != 1){ 
                 // 阻止ppid为0的进程以及消失的进程
                 // 阻止Idle进程
                 // 阻止sleep进程
                 // 阻止umask非0022的进程
-                procList->head.next = node->next; // 去除消失的进程结点
-                if(node != NULL){
-                    free(node); // 释放内存(可能导致释放两次)
-                    node = NULL;
+                tailptr->next = NULL; // &procList->tail; // 去除消失的进程结点
+                if(tailptr->next == NULL){// &procList->tail){
+                    free(node); // 释放内存
+                    node = NULL; // 不置为null可能导致释放两次
                 }
             }else{
                 procNum++;
+                tailptr = tailptr->next; // 只有完全获取proc信息后才移动尾指针, 否则会导致出现冗余错误信息
             }
         }
         strcpy(dirstr, "/proc/");
         state = ' '; // 重置状态
     }
-    printf("\nProcNum : %d\n\n", procNum);
-    
+
+    procList->number = procNum; // 写入进程总数
+
     // 关闭文件夹读写
     closedir(dir);
+
     return procList;
 }
 
--- m1_pstree/pstree.h
+++ m1_pstree/pstree.h
@@ -3,23 +3,35 @@
 
 #define MAX_LEN 50
 
-void showVersion();
-int pstree(int showPid, int pidSort);
-void procTree(int showPid, int pidSort);
-struct Node* getProc();
-
 // 定义进程结点信息
 typedef struct proc{
     int pid;
     int ppid;
     char name[MAX_LEN];
     struct proc *next;
+    struct proc *nextLevel;
 }procNode;
 
 // 定义进程链表头尾结点
 struct Node{
     procNode head;
     procNode tail;
+    int number;
 };
+
+// 定义层级链表控制结构
+// typedef struct treeLevel{
+//     procNode head;
+//     procNode tail;
+//     struct treeLevel *nextLevel;
+//     int number;
+// }treeLevel;
+
+void showVersion();
+int pstree(int showPid, int pidSort);
+void procTree(int showPid, int pidSort);
+void formationTree(struct Node* ptr, procNode* currentProc);
+void printTree(procNode* root, int deepth);
+struct Node* getProc();
 
 #endif
